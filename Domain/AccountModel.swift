2e3269fb3752b59f5d1e529d82c526ab0fa7cfdb
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-09 19:14:39 -0300

    add AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
---
+++ Domain/AddAccount.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
