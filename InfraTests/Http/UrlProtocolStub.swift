d08f173468241158048f4a7c602f045df992bc87
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-12 15:43:49 -0300

    move UrlProtocolStub to its own file

---
+++ InfraTests/Http/UrlProtocolStub.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var response: HTTPURLResponse?
+    static var error: Error?
+
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
+    }
+
+    override open class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override open func startLoading() {
+        UrlProtocolStub.emit?(request)
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override open func stopLoading() {}
+}
