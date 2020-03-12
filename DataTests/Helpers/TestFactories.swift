b9ef74234ca3d0e02cc2aa2a7eee81d8a917b676
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-12 13:29:08 -0300

    ensure AlamofireAdapter calls Alamofire with correct data

--- DataTests/Helpers/TestFactories.swift
+++ DataTests/Helpers/TestFactories.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data {
     return Data("invalid_data".utf8)
 }
 
+func makeValidData() -> Data {
+    return Data("{\"name\":\"Rodrigo\"}".utf8)
+}
+
 func makeUrl() -> URL {
     return URL(string: "http://any-url.com")!
 }
--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -8,8 +8,9 @@ class AlamofireAdapter {
         self.session = session
     }
 
-    func post(to url: URL) {
-        session.request(url, method: .post).resume()
+    func post(to url: URL, with data: Data?) {
+        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
@@ -20,11 +21,12 @@ class AlamofireAdapterTests: XCTestCase {
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         let sut = AlamofireAdapter(session: session)
-        sut.post(to: url)
+        sut.post(to: url, with: makeValidData())
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observeRequest { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
+            XCTAssertNotNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
