7ad33e85991e73804a14606465b522cf1d341276
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-12 15:06:16 -0300

    move expectation to post completion handler

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -55,7 +55,6 @@ class AlamofireAdapterTests: XCTestCase {
 
  valido
  ok ok x
- x x ok
 
  invalido
  ok ok ok
@@ -79,13 +78,12 @@ extension AlamofireAdapterTests {
 
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
         let sut = makeSut()
-        sut.post(to: url, with: data) { _ in }
         let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
-            action(request)
-            exp.fulfill()
-        }
+        sut.post(to: url, with: data) { _ in exp.fulfill() }
+        var request: URLRequest?
+        UrlProtocolStub.observeRequest { request = $0 }
         wait(for: [exp], timeout: 1)
+        action(request!)
     }
 }
 
