2f7f2a57505c711febdc8b0168209b77eef1a580
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-17 14:44:59 -0300

    move SignUpViewModel to its own file

--- Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -48,17 +48,3 @@ public final class SignUpPresenter {
         return nil
     }
 }
-
-public struct SignUpViewModel {
-    public var name: String?
-    public var email: String?
-    public var password: String?
-    public var passwordConfirmation: String?
-
-    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String? = nil) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
---
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public struct SignUpViewModel {
+    public var name: String?
+    public var email: String?
+    public var password: String?
+    public var passwordConfirmation: String?
+
+    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String? = nil) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
+}
