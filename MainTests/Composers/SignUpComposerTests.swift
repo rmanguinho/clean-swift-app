44ff96cefbe072c659f62b8d0e8428433909f06c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-20 17:25:23 -0300

    rename test file

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -1,11 +1,8 @@
 import XCTest
 import Main
 
-class SignUpIntegrationTests: XCTestCase {
+class SignUpComposerTests: XCTestCase {
     func test_ui_presentation_integration() {
-        debugPrint("=====================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("=====================")
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
