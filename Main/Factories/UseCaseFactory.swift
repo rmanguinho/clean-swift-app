af9cf0735648ba2d8a3e7d5f46734023da2dedc3
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-03-20 14:02:06 -0300

    improve code readability

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -4,9 +4,14 @@ import Infra
 import Domain
 
 final class UseCaseFactory {
+    private static let httpClient = AlamofireAdapter()
+    private static let apiBaseUrl = "https://clean-node-api.herokuapp.com/api"
+
+    private static func makeUrl(path: String) -> URL {
+        return URL(string: "\(apiBaseUrl)/\(path)")!
+    }
+
     static func makeRemoteAddAccount() -> AddAccount {
-        let alamofireAdapter = AlamofireAdapter()
-        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
-        return RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        return RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
     }
 }
