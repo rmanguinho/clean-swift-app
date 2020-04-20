adc9b385debd726161f5a758e3ffc3290fbb6fa1
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-20 14:29:38 -0300

    add Authentication UseCase

---
+++ Domain/UseCases/Authentication.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public protocol Authentication {
+    typealias Result = Swift.Result<AccountModel, DomainError>
+    func add(authenticationModel: AuthenticationModel, completion: @escaping (Result) -> Void)
+}
+
+public struct AuthenticationModel: Model {
+    public var email: String
+    public var password: String
+
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
