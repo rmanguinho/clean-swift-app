08907f975d0b5bb74882cd2446ccacc9a615243c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-20 11:20:49 -0300

    remove SignUpMapper to use it inside of SignUpViewModel

--- Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -19,7 +19,7 @@ public final class SignUpPresenter {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
         } else {
             loadingView.display(viewModel: LoadingViewModel(isLoading: true))
-            addAccount.add(addAccountModel: SignUpMapper.toAddAccountModel(viewModel: viewModel)) { [weak self] result in
+            addAccount.add(addAccountModel: viewModel.toAddAccountModel()) { [weak self] result in
                 guard let self = self else { return }
                 self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
                 switch result {
--- Presentation/Presenters/SignUp/SignUpViewModel.swift
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -13,4 +13,8 @@ public struct SignUpViewModel: Model {
         self.password = password
         self.passwordConfirmation = passwordConfirmation
     }
+
+    public func toAddAccountModel() -> AddAccountModel {
+        return AddAccountModel(name: name!, email: email!, password: password!, passwordConfirmation: passwordConfirmation!)
+    }
 }
