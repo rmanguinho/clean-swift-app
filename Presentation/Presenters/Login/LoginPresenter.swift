6d3292391c62808e768c06160775cb5113325e84
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-20 16:06:54 -0300

    ensure LoginPresenter completes with error if Authentication completes with error

--- Presentation/Presenters/Login/LoginPresenter.swift
+++ Presentation/Presenters/Login/LoginPresenter.swift
@@ -16,7 +16,13 @@ public final class LoginPresenter {
         if let message = validation.validate(data: viewModel.toJson()) {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
         } else {
-            authentication.auth(authenticationModel: viewModel.toAuthenticationModel()) { _ in }
+            authentication.auth(authenticationModel: viewModel.toAuthenticationModel()) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .failure: self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes."))
+                case .success: break
+                }
+            }
         }
     }
 }
--- PresentationTests/Presenters/Login/LoginPresenterTests.swift
+++ PresentationTests/Presenters/Login/LoginPresenterTests.swift
@@ -31,6 +31,20 @@ class LoginPresenterTests: XCTestCase {
         sut.login(viewModel: makeLoginViewModel())
         XCTAssertEqual(authenticationSpy.authenticationModel, makeAuthenticationModel())
     }
+
+    func test_login_should_show_generic_error_message_if_authentication_fails() {
+        let alertViewSpy = AlertViewSpy()
+        let authenticationSpy = AuthenticationSpy()
+        let sut = makeSut(alertView: alertViewSpy, authentication: authenticationSpy)
+        let exp = expectation(description: "waiting")
+        alertViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes."))
+            exp.fulfill()
+        }
+        sut.login(viewModel: makeLoginViewModel())
+        authenticationSpy.completeWithError(.unexpected)
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension LoginPresenterTests {
