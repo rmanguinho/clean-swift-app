fc986840f2de55941b97525b03896de86e9aad5a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-27 12:04:10 -0300

    change NavigationController init

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -7,10 +7,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        let nav = NavigationController()
         let httpClient = makeAlamofireAdapter()
         let addAccount = makeRemoteAddAccount(httpClient: httpClient)
         let signUpController = makeSignUpController(addAccount: addAccount)
-        let nav = NavigationController(rootViewController: signUpController)
+        nav.setRootViewController(signUpController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
--- UI/Components/NavigationController.swift
+++ UI/Components/NavigationController.swift
@@ -2,14 +2,18 @@ import Foundation
 import UIKit
 
 public final class NavigationController: UINavigationController {
-    public override init(rootViewController: UIViewController) {
-        super.init(rootViewController: rootViewController)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         setup()
     }
 
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
+    }
+
+    public convenience init() {
+        self.init(nibName: nil, bundle: nil)
     }
 
     private func setup() {
@@ -18,5 +22,13 @@ public final class NavigationController: UINavigationController {
         navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationBar.isTranslucent = false
         navigationBar.barStyle = .black
+    }
+
+    public func setRootViewController(_ viewController: UIViewController) {
+        setViewControllers([viewController], animated: true)
+    }
+
+    public func pushViewController(_ viewController: UIViewController) {
+        pushViewController(viewController, animated: true)
     }
 }
