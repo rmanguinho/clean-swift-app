0cbab80f49aafb6e16e2071455c2aa7a26b4fb14
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-27 12:32:28 -0300

    change Composition to start with WecomeViewController

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -4,14 +4,27 @@ import UI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
+    private let signUpFactory: () -> SignUpViewController = {
+        let alamofireAdapter = makeAlamofireAdapter()
+        let remoteAddAccount = makeRemoteAddAccount(httpClient: alamofireAdapter)
+        return makeSignUpController(addAccount: remoteAddAccount)
+    }
+
+    private let loginFactory: () -> LoginViewController = {
+        let alamofireAdapter = makeAlamofireAdapter()
+        let remoteAuthentication = makeRemoteAuthentication(httpClient: alamofireAdapter)
+        return makeLoginController(authentication: remoteAuthentication)
+    }
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let nav = NavigationController()
-        let httpClient = makeAlamofireAdapter()
-        let addAccount = makeRemoteAddAccount(httpClient: httpClient)
-        let signUpController = makeSignUpController(addAccount: addAccount)
-        nav.setRootViewController(signUpController)
+        let welcomeRouter = WelcomeRouter(nav: nav, loginFactory: loginFactory, signUpFactory: signUpFactory)
+        let welcomeViewController = WelcomeViewController.instantiate()
+        welcomeViewController.signUp = welcomeRouter.gotoSignUp
+        welcomeViewController.login = welcomeRouter.gotoLogin
+        nav.setRootViewController(welcomeViewController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
--- UI/Components/NavigationController.swift
+++ UI/Components/NavigationController.swift
@@ -2,6 +2,8 @@ import Foundation
 import UIKit
 
 public final class NavigationController: UINavigationController {
+    private var currentViewController: UIViewController?
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
@@ -26,9 +28,17 @@ public final class NavigationController: UINavigationController {
 
     public func setRootViewController(_ viewController: UIViewController) {
         setViewControllers([viewController], animated: true)
+        currentViewController = viewController
+        hideBackButtonText()
     }
 
     public func pushViewController(_ viewController: UIViewController) {
         pushViewController(viewController, animated: true)
+        currentViewController = viewController
+        hideBackButtonText()
+    }
+
+    public func hideBackButtonText() {
+        currentViewController?.navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
     }
 }
