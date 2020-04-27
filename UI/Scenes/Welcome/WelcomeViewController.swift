b822b89eac3d6e61d8329ad3ce9289c149503d02
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-27 10:47:14 -0300

    ensure WelcomeViewController calls login when button is tapped

---
+++ UI/Scenes/Welcome/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+
+public final class WelcomeViewController: UIViewController, Storyboarded {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+
+    public var login: (() -> Void)?
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+
+    private func configure() {
+        title = "4Dev"
+        loginButton?.layer.cornerRadius = 5
+        loginButton?.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        hideKeyboardOnTap()
+    }
+
+    @objc private func loginButtonTapped() {
+        login?()
+    }
+}
---
+++ UITests/Scenes/Welcome/WelcomeViewControllerTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import UIKit
+@testable import UI
+
+class WelcomeViewControllerTests: XCTestCase {
+    func test_loginButton_calls_login_on_tap() {
+        let (sut, buttonSpy) = makeSut()
+        sut.loginButton?.simulateTap()
+        XCTAssertEqual(buttonSpy.clicks, 1)
+    }
+}
+
+extension WelcomeViewControllerTests {
+    func makeSut() -> (sut: WelcomeViewController, buttonSpy: ButtonSpy) {
+        let buttonSpy = ButtonSpy()
+        let sut = WelcomeViewController.instantiate()
+        sut.login = buttonSpy.onClick
+        sut.loadViewIfNeeded()
+        checkMemoryLeak(for: sut)
+        return (sut, buttonSpy)
+    }
+
+    class ButtonSpy {
+        var clicks = 0
+
+        func onClick() {
+            clicks += 1
+        }
+    }
+}
