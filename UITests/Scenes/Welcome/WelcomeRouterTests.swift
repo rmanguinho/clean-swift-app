7f2f418ac374654c6ab21ee7638cf3aa6a8e7a00
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-27 12:20:58 -0300

    ensure WelcomeRouter calls nav with signUpFactory on gotoSignUp

--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -5,14 +5,20 @@ import UI
 public final class WelcomeRouter {
     private let nav: NavigationController
     private let loginFactory: () -> LoginViewController
+    private let signUpFactory: () -> SignUpViewController
 
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController) {
+    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController) {
         self.nav = nav
         self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
     }
 
     public func gotoLogin() {
         nav.pushViewController(loginFactory())
+    }
+
+    public func gotoSignUp() {
+        nav.pushViewController(signUpFactory())
     }
 }
 
@@ -23,13 +29,21 @@ class WelcomeRouterTests: XCTestCase {
         XCTAssertEqual(nav.viewControllers.count, 1)
         XCTAssertTrue(nav.viewControllers[0] is LoginViewController)
     }
+
+    func test_gotoSignUp_calls_nav_with_correct_vc() {
+        let (sut, nav) = makeSut()
+        sut.gotoSignUp()
+        XCTAssertEqual(nav.viewControllers.count, 1)
+        XCTAssertTrue(nav.viewControllers[0] is SignUpViewController)
+    }
 }
 
 extension WelcomeRouterTests {
     func makeSut() -> (sut: WelcomeRouter, nav: NavigationController) {
         let loginFactorySpy = LoginFactorySpy()
+        let signUpFactorySpy = SignUpFactorySpy()
         let nav = NavigationController()
-        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin)
+        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin, signUpFactory: signUpFactorySpy.makeSignUp)
         return (sut, nav)
     }
 }
@@ -39,6 +53,12 @@ extension WelcomeRouterTests {
     class LoginFactorySpy {
         func makeLogin() -> LoginViewController {
             return LoginViewController.instantiate()
+        }
+    }
+
+    class SignUpFactorySpy {
+        func makeSignUp() -> SignUpViewController {
+            return SignUpViewController.instantiate()
         }
     }
 }
